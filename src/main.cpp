74f7694a9baad6569175d9920b82299c45e9132e
matija <vodokotlic.vudukotlic@gmail.com>
2017-05-24 21:13:59 +0200

    rotary encoder

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,13 @@
 #include "mbed.h"
 #include "rtos.h"
+
 #include "TextLCD.h"
 #include "DebounceIn.h"
-#include "StepperMotorUni"
-#include "RPG"
-#include "KeyPad"
+#include "StepperMotorUni.h"
+#include "RPG.h"
+#include "KeyPad.h"
 
-
+RPG rotary(PA_1,PC_3,PC_1);
 DebounceIn btn(PA_0);
 AnalogIn pot(PA_1);
 TextLCD lcd(PD_0, PD_2, PD_4, PD_6, PB_3, PB_5); // rs, e, d4-d7
@@ -25,10 +26,19 @@ int main() {
           leds[i] = new DigitalOut(PinName(PE_8+i));
       }
 int p =0;
+int dir = 0;
 bool down = false;
-int delay = 50;
+int delay = 5;
       while (true) {
-
+        dir = rotary.dir();
+        Thread::wait(delay);
+        p += dir;
+        lcd.printf("%d        \n                ",p,0,0);
+        if (rotary.pb())
+        {
+            p = 0;
+        }
+        /*
           while(btn){
             if (!down){
               t.start();
@@ -42,6 +52,7 @@ int delay = 50;
           t.stop();
           down = false;
           Thread::wait(delay);
+          */
         //  lcd.printf("    \n");
 /*
       for ( int i=0; i<8; i++ ){
